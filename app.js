415076c88ec5155cb4866c902a2ac9a866b91825
Shirley Su <shirley2452452453@gmail.com>
2023-03-06 12:43:27 -0800

    test

--- app.js
+++ app.js
@@ -146,7 +146,7 @@ function callOpenApi(senderPsid, requestText) {
     model: "text-davinci-003",
     prompt: "Please reply to the following: " + requestText,
   });
-  console.log('after call await v15');
+  console.log('after call await v16');
   completion.then(async (result) => {
     console.log('data v15');
     const data = result.data;
