2379fee07024212d447b72be07e95117d328200c
Shirley Su <shirley2452452453@gmail.com>
2023-03-05 22:56:34 -0800

    test

--- app.js
+++ app.js
@@ -49,13 +49,17 @@ app.get('/webhook', (req, res) => {
   // Parse the query params
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
+  // let token = VERIFY_TOKEN;
   let challenge = req.query['hub.challenge'];
+  console.log(mode);
+  console.log(token);
+  console.log(challenge);
 
   // Checks if a token and mode is in the query string of the request
   if (mode && token) {
 
     // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    if (mode === 'subscribe') {//} && token === VERIFY_TOKEN) {
 
       // Responds with the challenge token from the request
       console.log('WEBHOOK_VERIFIED');
@@ -65,6 +69,9 @@ app.get('/webhook', (req, res) => {
       // Responds with '403 Forbidden' if verify tokens do not match
       res.sendStatus(403);
     }
+  } else {
+    // Responds with '403 Forbidden' if verify tokens do not match
+    res.sendStatus(404);
   }
 });
 
@@ -100,7 +107,7 @@ app.post('/webhook', (req, res) => {
   } else {
 
     // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
+    res.sendStatus(200);
   }
 });
 
