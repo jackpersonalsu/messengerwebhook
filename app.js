3de65c1bd059cdf8d3ac485e089f220d2b387b55
Shirley Su <shirley2452452453@gmail.com>
2023-03-06 11:18:27 -0800

    test

--- app.js
+++ app.js
@@ -141,7 +141,7 @@ async function callOpenApi(senderPsid, requestText) {
   const openai = new OpenAIApi(configuration);
   console.log('open ai');
   console.log(openai);
-  const completion = openai.createCompletion({
+  const completion = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: "Please reply to the following: " + requestText,
   });
