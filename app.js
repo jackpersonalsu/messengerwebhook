0805cb2e718ad96738322d37cb1dea8f614473b9
Shirley Su <shirley2452452453@gmail.com>
2023-03-09 20:21:47 -0800

    test

--- app.js
+++ app.js
@@ -54,11 +54,11 @@ app.post('/webhook', (req, res) => {
     });
 
     // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
+    return res.status(200).send('EVENT_RECEIVED');
   } else {
 
     // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(200);
+    return res.sendStatus(200);
   }
 });
 
