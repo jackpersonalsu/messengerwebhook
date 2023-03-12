773b896aed66cd696351bff6c47b668652d2b5ae
Shirley Su <shirley2452452453@gmail.com>
2023-03-12 13:07:39 -0700

    update

--- app.js
+++ app.js
@@ -200,14 +200,14 @@ function callSendAPI(senderPsid, response) {
   }, (err, _res, _body) => {
     if (!err) {
       console.log('Message sent!');
-      request({
-        'uri': 'https://graph.facebook.com/v2.6/me/messages',
-        'qs': { 'access_token': PAGE_ACCESS_TOKEN },
-        'method': 'POST',
-        'json': requestBody2}, (e1, _, _) => {
-          console.log('second message sent');
-          console.log(err);
-        });
+      // request({
+      //   'uri': 'https://graph.facebook.com/v2.6/me/messages',
+      //   'qs': { 'access_token': PAGE_ACCESS_TOKEN },
+      //   'method': 'POST',
+      //   'json': requestBody2}, (e1, _, _) => {
+      //     console.log('second message sent');
+      //     console.log(err);
+      //   });
     } else {
       console.error('Unable to send message:' + err);
     }
