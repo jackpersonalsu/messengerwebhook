799557272bb529f86702c6c78f37477698a772a3
Shirley Su <shirley2452452453@gmail.com>
2023-03-16 23:47:10 -0700

    test

--- app.js
+++ app.js
@@ -184,6 +184,7 @@ function callSendAPI(senderPsid, response, requestText) {
     "prompt": prompt,
     "temperature": 0, 
     "max_tokens": 128,
+    "session": senderPsid
   };
   console.log('sending request');
   request({
